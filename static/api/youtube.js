633b52b5b5416655b5a6958cc6ef275b8b42fa37
Dearam <80329368+Dearam@users.noreply.github.com>
2021-08-07 14:34:16 +0530

    Create youtube.js

---
+++ static/api/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyAwDmZakGzdDfgXhPtX5e6NIWyBqI6Y-lw'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults : 5,
+        key: KEY
+    }
+});
